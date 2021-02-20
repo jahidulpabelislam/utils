4488bf2f2d4ca9908326f5b86fe631f8cf17b48a
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2021-02-20 00:50:54 +0000

    Add singleton trait

---
+++ src/Singleton.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace JPI\Utilities;
+
+trait Singleton
+{
+
+    protected static $instance = null;
+
+    /**
+     * @return static
+     */
+    public static function get() {
+        if (static::$instance === null) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * Singleton constructor.
+     *
+     * By default don't allow creating instances of the class outside the getter
+     */
+    protected function __construct()
+    {
+    }
+
+}
