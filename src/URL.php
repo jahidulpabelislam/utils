c6fbc0e9babc9a0fd52f4f4c896e765898f89a99
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2022-09-28 23:48:45 +0100

    Add proper builder for URL

--- src/URL.php
+++ src/URL.php
@@ -42,4 +42,118 @@ class URL {
 
         return "$url/";
     }
+
+    protected $scheme;
+    protected $host;
+    protected $path;
+    protected $params = [];
+    protected $fragment;
+
+    protected $addTrailingSlash = true;
+
+    public function __construct(string $url = null) {
+        if (!$url) {
+            return;
+        }
+
+        $isProtocolRelative = strpos($url, '//') === 0;
+
+        if ($isProtocolRelative) {
+            $url = "https:$url";
+        }
+
+        $parsed = parse_url($url);
+
+        if (!$isProtocolRelative) {
+            $this->setScheme($parsed["scheme"] ?? null);
+        }
+
+        $this->setHost($parsed["host"] ?? null);
+        $this->setPath($parsed["path"] ?? null);
+
+        if (isset($parsed["query"])) {
+            parse_str($parsed["query"], $params);
+            $this->setParams($params);
+        }
+
+        $this->setFragment($parsed["fragment"] ?? null);
+    }
+
+    public function setScheme(string $scheme = null): URL {
+        $this->scheme = $scheme;
+        return $this;
+    }
+
+    public function setHost(string $host = null): URL {
+        $this->host = $host;
+        return $this;
+    }
+
+    public function setPath(string $path = null): URL {
+        if ($path === '/') {
+            $path = null;
+        }
+
+        $this->path = $path;
+        return $this;
+    }
+
+    public function addPath(string $path): URL {
+        if (!$this->path) {
+            return $this->setPath($path);
+        }
+        return $this->setPath(static::addTrailingSlash($this->path) . $path);
+    }
+
+    public function setParams(array $params): URL {
+        $this->params = $params;
+        return $this;
+    }
+
+    public function setFragment(string $fragment = null): URL {
+        $this->fragment = $fragment;
+        return $this;
+    }
+
+    public function build(): string {
+        $string = "";
+
+        if ($this->scheme) {
+            $string = "$this->scheme:";
+        }
+
+        if ($this->host) {
+            $string .= "//$this->host";
+        }
+
+        if ($this->path) {
+            $string .= static::addLeadingSlash($this->path);
+
+            if ($this->addTrailingSlash) {
+                $string = static::addTrailingSlash($string);
+            }
+        }
+
+        if ($this->params) {
+            if ($this->path) {
+                $string = static::addTrailingSlash($string);
+            } else {
+                $string .= "/";
+            }
+            $string .= "?" . http_build_query($this->params);
+        }
+        else if (!$this->scheme && !$this->host && !$this->path) {
+            $string = "/";
+        }
+
+        if ($this->fragment) {
+            $string .= "#$this->fragment";
+        }
+
+        return $string;
+    }
+
+    public function __toString(): string {
+        return $this->build();
+    }
 }
