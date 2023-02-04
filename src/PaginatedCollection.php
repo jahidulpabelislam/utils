0912bd9533cb7c082bcd35925b078c4f1428d438
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2023-02-04 22:29:09 +0000

    Add paginated collection

---
+++ src/PaginatedCollection.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JPI\Utils;
+
+/**
+ * Collection that is from a paginated result.
+ */
+class PaginatedCollection extends Collection {
+
+    use ImmutableCollectionTrait;
+
+    /**
+     * @var int
+     */
+    protected $totalCount;
+
+    /**
+     * @var int
+     */
+    protected $limit;
+
+    /**
+     * @var int
+     */
+    protected $page;
+
+    /**
+     * @param array $items
+     * @param int $totalCount
+     * @param int $limit
+     * @param int $page
+     */
+    public function __construct(array $items, int $totalCount, int $limit, int $page) {
+        $this->items = $items;
+        $this->count = count($this->items);
+        $this->totalCount = $totalCount;
+        $this->limit = $limit;
+        $this->page = $page;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalCount(): int {
+        return $this->totalCount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLimit(): int {
+        return $this->limit;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPage(): int {
+        return $this->page;
+    }
+}
